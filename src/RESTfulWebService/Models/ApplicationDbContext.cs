5bd6e25224e4914acd581c7487413cd946e2d250
Alexandra Luneva <Luneva.AV@mail.ru>
2021-12-04 19:49:22 +0700

    Add a Data Model

---
+++ src/RESTfulWebService/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTfulWebService.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opts)
+            : base(opts) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
---
+++ src/RESTfulWebService/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace RESTfulWebService.Models
+{
+    public class Category
+    {
+        public long CategoryId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
